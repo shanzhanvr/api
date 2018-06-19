614b2d29ca6560e3286b8070c8546e0a885beaf9
wujunjun <wujunjun@dongfangfuli.com>
2018-06-19 20:50:46 +0800

    [api接口]

--- routes/api/sms/sms.php
+++ routes/api/sms/sms.php
@@ -8,8 +8,9 @@
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Api\V1\Controllers\Sms','middleware' => ['api.vrauth']],function ($api) {
+        $api->get('sms/smssend', 'SmsController@smsSend');//短信发送 验证token
         $api->group(['middleware' => ['before' => 'jwt.auth']], function ($api){
-            $api->get('sms/smssend', 'SmsController@smsSend');//短信发送 验证token
+
         });
     });
 });
