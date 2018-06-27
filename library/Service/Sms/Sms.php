6eb5b3254f4dd457d7cabdbc66d5c6111a718cfc
zhaoxinying <1591750057@qq.com>
2018-06-27 12:05:11 +0800

    [master短信接口]

--- library/Service/Sms/Sms.php
+++ library/Service/Sms/Sms.php
@@ -59,7 +59,8 @@ class Sms implements Contract {
         $params = $this->getFileds($phone,$num);
         $result = $this->sms->request($this->accesskeyid,$this->accesskeysecret,$this->domain,array_merge($params,array("RegionId" => "cn-hangzhou", "Action" => "SendSms", "Version" => "2017-05-25",)));
         $result = \helper::object2array($result);
-        $result['outId'] = $params['OutId'];
+        $TemplateParam = !is_null($params['TemplateParam'])?json_decode($params['TemplateParam'],true) :'';
+        $result['outId'] = $TemplateParam['code'];
         return $result;
     }
 
