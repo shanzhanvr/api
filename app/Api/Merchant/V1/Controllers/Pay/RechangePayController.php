9b04c2ed99b70e09e882f5fd86edf5d881c3fcd5
wujunjun <wujunjun@dongfangfuli.com>
2018-08-28 10:21:43 +0800

    [maste]

--- app/Api/Merchant/V1/Controllers/Pay/RechangePayController.php
+++ app/Api/Merchant/V1/Controllers/Pay/RechangePayController.php
@@ -36,124 +36,130 @@ class RechangePayController extends BaseController {
         $this->blog = BLog::get_instance();
     }
     //下单接口
-    public function alipayPay(){
-        $input = Input::all();
-        $user = JWTAuth::parseToken()->authenticate();
-        $redis = TMRedisCacheMgr::getInstance();
-        //调用枷锁服务
-        if(!$redis->setLock($user->mobile,2)){
-            return JsonResponse::error(0,'请勿频繁访问');
-        }
-        $validator = Validator::make(
-            $input,
-            ['amount' => 'required|Numeric', 'rechargeType' => 'required|'.Rule::in(RechangeConst::desc())],
-            ['amount.required' => '充值金额不能为空', 'amount.Numeric' => '充值金额为整数', 'rechargeType.required'=>'请选择充值方式', 'rechargeType.in' => '请选择充值方式']
-        );
-        if ($validator->fails()) {
-            return JsonResponse::error(0, '验证失败', $validator->errors()->toArray());
-        }
-        $rechargeType = '';
-        if ($input['rechargeType'] == 1) {
-            $rechargeType = PayConst::wechatnative;
-        } elseif ($input['rechargeType'] == 2) {
-            $rechargeType = PayConst::alipay;
-        }
-
-        $applyPay = new Apply();
-        $this->blog->log('merchant_'.$user->mobile,'-----start---发起的支付----参数'.json_encode(Input::all()));
-        $response = $applyPay::unifiedOrder(\helper::getOrderno(), $input['amount'], $this->body, $rechargeType);
-        $this->blog->log('merchant_'.$user->mobile,'-----调用支付接口---发起的支付'.json_encode($response));
-        if ($response['code'] && !empty($response['data'])) {
-            try {
-                DB::beginTransaction();//开启事物
-                $payData = \helper::object2array($response['data']);
-                //产生预支付订单
-                $rechange = new RechargeModel();
-                $rechange->merchantId = $user->id;
-                $account = AccountModel::query()->where('merchantId', $user->id)->first();
-                $rechange->accountId = $account->id;
-                $rechange->amount = $this->getYuanFromFen($input['amount']);
-                $rechange->rechargeType = $input['rechargeType'];
-                $rechange->rechargeSerialNo = $payData['merchantOrderNo'];
-                $rechange->outTradeNo = $payData['tradeOrderNo'];
-                $rechange->status = RechangeConst::RECHANGE_ACTION_STATUCT_ING;
-                $rechange->ip = \helper::getClientIp();
-                $rechange->save();
-                //交易记录
-                $recode = new TradeRecodeModel();
-                $recode->merchantId = $user->id;
-                $recode->accountId = $rechange->accountId;
-                $recode->recodeType = TradeTypeConst::OBJECT_ACTION_TYPE_RECHANGE;
-                $recode->recodeSerialNo = $payData['merchantOrderNo'];
-                $recode->outTradeNo = $payData['tradeOrderNo'];
-                $recode->preBlance = $account->blance;
-                $recode->blance = (int)($account->blance + $this->getYuanFromFen($input['amount']));
-                $recode->preAmount = $account->amount;//变动前可提现金额
-                $recode->amount = $account->amount;//变动后可提现金额
-                $recode->tradeaMount = $this->getYuanFromFen($input['amount']);
-                $recode->status = RechangeConst::RECHANGE_ACTION_STATUCT_ING;
-                $recode->ip = \helper::getClientIp();
-                $recode->save();
-                DB::commit();
-                return JsonResponse::success(['qrcodeUrl' => $payData['qrcodeUrl'],'merchantOrderNo'=>$payData['merchantOrderNo']]);
-            } catch (\Exception $exception) {
-                $this->blog->log('merchant_'.$user->mobile,'-----调用支付接口---产生异常'.json_encode($exception->getMessage()));
-                DB::rollBack();
-                return JsonResponse::error(0, '网络错误！请稍后重试');
+    public function alipayPay() {
+        try {
+            $input = Input::all();
+            $user = JWTAuth::parseToken()->authenticate();
+            $redis = TMRedisCacheMgr::getInstance();
+            //调用枷锁服务
+            if (!$redis->setLock($user->mobile, 2)) {
+                return JsonResponse::error(0, '请勿频繁访问');
             }
-        } else {
-            $this->blog->log('merchant_'.$user->mobile,'-----调用支付接口---产生异常'.json_encode($response));
-            return JsonResponse::error(0, '网络错误!请稍后重试');
+            $validator = Validator::make(
+                $input, ['amount' => 'required|Numeric', 'rechargeType' => 'required|' . Rule::in(RechangeConst::desc())],
+                ['amount.required' => '充值金额不能为空', 'amount.Numeric' => '充值金额为整数', 'rechargeType.required' => '请选择充值方式', 'rechargeType.in' => '请选择充值方式']
+            );
+            if ($validator->fails()) {
+                return JsonResponse::error(0, '验证失败', $validator->errors()->toArray());
+            }
+            $rechargeType = '';
+            if ($input['rechargeType'] == 1) {
+                $rechargeType = PayConst::wechatnative;
+            } elseif ($input['rechargeType'] == 2) {
+                $rechargeType = PayConst::alipay;
+            }
+            $applyPay = new Apply(env('LEARNING_PAYAPP_MERCHANT_NOTIFY'));
+            $this->blog->log('merchant_' . $user->mobile, '-----start---发起的支付----参数' . json_encode(Input::all()));
+            $response = $applyPay::unifiedOrder(\helper::getOrderno(), $input['amount'], $this->body, $rechargeType);
+            DB::connection('db_vr_merchant')->beginTransaction();
+            $this->blog->log('merchant_' . $user->mobile, '-----调用支付接口---发起的支付' . json_encode($response));
+            if ($response['code'] && !empty($response['data'])) {
+                    $payData = \helper::object2array($response['data']);
+                    //产生预支付订单
+                    $rechange['merchantId'] = $user->id;
+                    $account = AccountModel::query()->where('merchantId', $user->id)->first();
+                    $rechange['accountId'] = $account->id;
+                    $rechange['amount'] = $this->getYuanFromFen($input['amount']);
+                    $rechange['rechargeType'] = $input['rechargeType'];
+                    $rechange['rechargeSerialNo'] = $payData['merchantOrderNo'];
+                    $rechange['outTradeNo'] = $payData['tradeOrderNo'];
+                    $rechange['status'] = RechangeConst::RECHANGE_ACTION_STATUCT_ING;
+                    $rechange['ip'] = \helper::getClientIp();
+                    $rechange['created_at'] = date('Y-m-d H:i:s');
+                    $rechange['updated_at'] = date('Y-m-d H:i:s');
+                    $rechargeId = DB::connection('db_vr_merchant')->table('recharge')->insertGetId($rechange);
+                    //交易记录
+                    $recode['merchantId'] = $user->id;
+                    $recode['accountId'] = $account->id;
+                    $recode['recodeType'] = TradeTypeConst::OBJECT_ACTION_TYPE_RECHANGE;
+                    $recode['recodeSerialNo'] = $payData['merchantOrderNo'];
+                    $recode['outTradeNo'] = $payData['tradeOrderNo'];
+                    $recode['preBlance'] = $account->blance;
+                    $recode['blance'] = (int)($account->blance + $this->getYuanFromFen($input['amount']));
+                    $recode['preAmount'] = $account->amount;//变动前可提现金额
+                    $recode['amount'] = $account->amount;//变动后可提现金额
+                    $recode['tradeaMount'] = $this->getYuanFromFen($input['amount']);
+                    $recode['status'] = RechangeConst::RECHANGE_ACTION_STATUCT_ING;
+                    $recode['ip'] = \helper::getClientIp();
+                    $recode['created_at'] = date('Y-m-d H:i:s');
+                    $recode['updated_at'] = date('Y-m-d H:i:s');
+                    $recodeId = DB::connection('db_vr_merchant')->table('recode')->insertGetId($recode);
+                    if($rechargeId && $recodeId){
+                        DB::connection('db_vr_merchant')->commit();
+                        return JsonResponse::success(['qrcodeUrl' => $payData['qrcodeUrl'], 'merchantOrderNo' => $payData['merchantOrderNo']]);
+                    }
+            } else {
+                $this->blog->log('merchant_' . $user->mobile, '-----调用支付接口---产生异常' . json_encode($response));
+                return JsonResponse::error(0, '网络错误!请稍后重试');
+            }
+        }catch (\Exception $e){
+            DB::connection('db_vr_merchant')->rollBack();
+            return JsonResponse::error(0,'网络繁忙!请稍后重试');
         }
     }
 
     //支付回调接口
     public function receivePay(){
+        //开启事物
+        DB::connection('db_vr_merchant')->beginTransaction();
+        try {
         $responseData = file_get_contents('php://input');
         $this->blog->log('receivepay','支付接口回调解密前的报文:' . $responseData);
-        if (!empty($responseData)) {
-            $responseData = json_decode($responseData, true);
-            if (isset($responseData['appId']) && env('LEARNING_PAYAPPID') == $responseData['appId']) {
-                $applyPay = new Apply(env('LEARNING_PAYAPP_NOTIFY'));
-                $aesKey = Rsa::privateDecrypt($responseData['encryptKey'], $applyPay::$privateKey);
-                $data = Aes::opensslDecrypt($responseData['encryptData'], $aesKey);
-                $this->blog->log('receivepay','支付接口回调解密后的报文:' . $data);
-                $data = json_decode($data, true);
-                //开启事物
-                try {
-                    DB::beginTransaction();
-                    $rechangeModel = RechargeModel::query()->where('outTradeNo', $data['tradeOrderNo'])->where('status', RechangeConst::RECHANGE_ACTION_STATUCT_ING)->first();
-                    $tradeModel = TradeRecodeModel::query()->where('outTradeNo', $data['tradeOrderNo'])->where('status', RechangeConst::RECHANGE_ACTION_STATUCT_ING)->first();
-                    $status = RechangeConst::RECHANGE_ACTION_STATUCT_SUCCESS;
-                    if ($rechangeModel && $tradeModel) {
-                        $rechangeModel->status = $status;
-                        $rechangeModel->respCode = $data['respCode'];
-                        $rechangeModel->respMsg = $data['respMsg'];
-                        $rechangeModel->payTime = $data['payTime'];
-                        $rechangeModel->save();
-                        $tradeModel->status = $status;
-                        $tradeModel->respCode = $data['respCode'];
-                        $tradeModel->respMsg = $data['respMsg'];
-                        $tradeModel->payTime = $data['payTime'];
-                        $tradeModel->save();
-                        if ($data['respCode'] == '0000') {
+            if (!empty($responseData)) {
+                $responseData = json_decode($responseData, true);
+                if (isset($responseData['appId']) && env('LEARNING_PAYAPPID') == $responseData['appId']) {
+                    $applyPay = new Apply(env('LEARNING_PAYAPP_NOTIFY'));
+                    $aesKey = Rsa::privateDecrypt($responseData['encryptKey'], $applyPay::$privateKey);
+                    $data = Aes::opensslDecrypt($responseData['encryptData'], $aesKey);
+                    $this->blog->log('receivepay','支付接口回调解密后的报文:' . $data);
+                    $data = json_decode($data, true);
+                    if ($data['respCode'] == '0000') {
+                        $rechangeModel = RechargeModel::query()->where('outTradeNo', $data['tradeOrderNo'])->where('status', RechangeConst::RECHANGE_ACTION_STATUCT_ING)->first();
+                        $tradeModel = TradeRecodeModel::query()->where('outTradeNo', $data['tradeOrderNo'])->where('status', RechangeConst::RECHANGE_ACTION_STATUCT_ING)->first();
+                        $status = RechangeConst::RECHANGE_ACTION_STATUCT_SUCCESS;
+                        if ($rechangeModel && $tradeModel) {
+                            $rechangeData['status'] = $status;
+                            $rechangeData['respCode'] = $data['respCode'];
+                            $rechangeData['respMsg'] = $data['respMsg'];
+                            $rechangeModel['payTime'] = $data['payTime'];
+                            $rechangeData['updated_at'] = date('Y-m-d H:i:s');
+                            $rechargeId = DB::connection('db_vr_merchant')->table('recharge')->where('outTradeNo', $data['tradeOrderNo'])->update($rechangeData);
+                            $tradeData['status'] = $status;
+                            $tradeData['respCode'] = $data['respCode'];
+                            $tradeData['respMsg'] = $data['respMsg'];
+                            $tradeData['payTime'] = $data['payTime'];
+                            $tradeData['updated_at'] = date('Y-m-d H:i:s');
+                            $recodeId = DB::connection('db_vr_merchant')->table('recode')->where('outTradeNo',$data['tradeOrderNo'])->update($tradeData);
                             $accountModel = AccountModel::find($rechangeModel->accountId);
-                            $accountModel->blance = $this->getSumamount($this->getYuanFromFen($data['amount']), $accountModel->blance);
-                            $accountModel->save();
+                            $accountId = DB::connection('db_vr_merchant')->table('account')->where('id',$rechangeModel->accountId)->update([
+                                'blance'=>$this->getSumamount($this->getYuanFromFen($data['amount']), $accountModel->blance),'updated_at'=>date('Y-m-d H:i:s')]);
+                            if($rechargeId && $recodeId && $accountId){
+                                DB::connection('db_vr_merchant')->commit();
+                                return 'SUCCESS';
+                            }
+                            DB::connection('db_vr_merchant')->rollBack();
+                            return 'SUCCESS';
                         }
-                        DB::commit();
-                        return 'SUCCESS';
                     }
-                } catch (\Exception $exception) {
-                    Log::info('支付接口回调error:' . json_encode($exception->getMessage()));
-                    DB::rollBack();
-                    return 'ERROR';
                 }
             } else {
                 return 'ERROR';
             }
+        } catch (\Exception $exception) {
+            Log::info('支付接口回调error:' . json_encode($exception->getMessage()));
+            DB::connection('db_vr_merchant')->rollBack();
+            return 'ERROR';
         }
-        return 'ERROR';
     }
     //支付回调订单检查
     public function checkOrder(){
--- routes/api/merchant/pay/rechange.php
+++ routes/api/merchant/pay/rechange.php
@@ -8,10 +8,10 @@
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', function ($api) {
     $api->group(['namespace' => 'App\Api\Merchant\V1\Controllers\Pay','middleware' => ['api.merchant']],function ($api) {
-        $api->POST('pay/receiver', 'RechangePayController@receivePay');//支付回调
+        $api->POST('merchant/pay/receiver', 'RechangePayController@receivePay');//支付回调
         $api->group(['middleware' => ['before' => 'jwt.auth']], function ($api){
-            $api->POST('pay/receivefront', 'RechangePayController@checkOrder');//检查支付订单
-            $api->POST('pay/unified', 'RechangePayController@alipayPay');//下单
+            $api->POST('merchant/pay/receivefront', 'RechangePayController@checkOrder');//检查支付订单
+            $api->POST('merchant/pay/unified', 'RechangePayController@alipayPay');//下单
         });
     });
 });
